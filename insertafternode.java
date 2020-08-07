5ec330f01ac13ba53b6df5ab76c0bf123d8e3b2b
Ishita120 <68339093+Ishita120@users.noreply.github.com>
2020-08-08 02:04:17 +0530

    Add files via upload

---
+++ insertafternode.java
@@ -0,0 +1,66 @@
+package data;
+
+public class insertafternode {
+
+			  public static Node<Integer> takeInput()
+				{
+					Node<Integer> head = null,tail=null;
+					Scanner x = new Scanner(System.in);
+					int data = x.nextInt();
+					while(data!=-1)
+					{
+						Node<Integer> newNode = new Node<Integer>(data);
+						
+						if(head==null)
+						{
+							head= newNode;
+							tail = newNode;
+						}
+						else
+						{	
+							tail.next= newNode; 
+							tail= newNode;
+						} 
+						
+						data = x.nextInt();
+					}
+					return head;
+				}
+				
+				public static Node<Integer> insert(Node<Integer> head, int data, int pos)
+				{
+						 Node<Integer> newNode= new Node<>(data);
+						 int i=0;
+						 Node<Integer> temp= head;
+						 
+						 if(pos==0)
+						 {
+							 newNode.next= head;
+							 head= newNode;
+							 return newNode; 
+						 }
+						 
+						 while(i<pos-1)
+						 {
+							 temp=temp.next;
+							 i++;
+						 }
+						 newNode.next= temp.next;
+						 temp.next= newNode;
+						 return head;	
+						 
+				}
+				
+				public static void main(String[] args) {
+					
+					Node<Integer> head = takeInput();
+					
+					insert(head, 5, 1);
+					
+					
+				}
+			  
+			}
+	}
+
+}
